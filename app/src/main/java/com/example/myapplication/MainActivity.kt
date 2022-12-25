560a2857fd0c777893dd284003c587d2baba8263
Muneeb Khalid <muneebkhalid0017@gmail.com>
2022-12-25 21:26:16 +0500

    aa

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -11,11 +11,6 @@ import kotlinx.android.synthetic.main.layout_two.*
 import kotlinx.android.synthetic.main.login2.*
 
 class MainActivity : AppCompatActivity() {
-
-    private lateinit var btnsend: Button
-    private lateinit var editusername: EditText
-    private lateinit var edituserpassword: EditText
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.login2)
@@ -29,11 +24,6 @@ class MainActivity : AppCompatActivity() {
             startActivity(intent)
         }
 
-        btnsend = findViewById(R.id.btnsend)
-        editusername = findViewById(R.id.editusername)
-        edituserpassword = findViewById(R.id.edituserpassword)
-
-
         btnsend.setOnClickListener {
             startActivity(
                 Intent(this, perfile_activity::class.java)
@@ -41,7 +31,6 @@ class MainActivity : AppCompatActivity() {
                     .putExtra("password", edituserpassword.text.toString())
             )
         }
-
 
     }
 }
--- app/src/main/java/com/example/myapplication/perfile_activity.kt
+++ app/src/main/java/com/example/myapplication/perfile_activity.kt
@@ -9,8 +9,6 @@ import android.widget.TextView
 import kotlinx.android.synthetic.main.activity_perfile.*
 
 class perfile_activity : AppCompatActivity() {
-
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_perfile)
